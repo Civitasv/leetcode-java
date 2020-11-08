a5005cef8b01e29b7a1a93f3b60ff3814c24eab9
husen <2309780282@qq.com>
2020-11-08 21:44:43 +0800

    Remove Element

---
+++ src/main/RemoveElement.java
@@ -0,0 +1,40 @@
+package main;
+
+/**
+ * Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+ * <p>
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * <p>
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ * <p>
+ * Clarification:
+ * <p>
+ * Confused why the returned value is an integer but your answer is an array?
+ * <p>
+ * Note that the input array is passed in by reference, which means a modification to the input array will be known to the caller as well.
+ * <p>
+ * Internally you can think of this:
+ */
+public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == val) {
+                int j = n - 1;
+                while (nums[j] == val && j > i) {
+                    j--;
+                    n--;
+                }
+                swap(nums, i, j);
+                n--;
+            }
+        }
+        return n;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
---
+++ src/test/RemoveElementTest.java
@@ -0,0 +1,17 @@
+package test;
+
+
+import main.RemoveElement;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+class RemoveElementTest {
+    @Test
+    void test() {
+        RemoveElement removeElement = new RemoveElement();
+        int[] arr = new int[]{0, 1, 2, 2, 3, 0, 4, 2};
+        System.out.println(removeElement.removeElement(arr, 2));
+        System.out.println(Arrays.toString(arr));
+    }
+}
