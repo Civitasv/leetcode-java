9431fc909b44abc13b1735b10bc82b7d4d62be5a
husen <2309780282@qq.com>
2020-11-25 13:08:38 +0800

    字符串空格替换

--- src/main/offer/ReplaceEmptyPlace.java
+++ src/main/offer/ReplaceEmptyPlace.java
@@ -5,6 +5,12 @@ package main.offer;
  */
 public class ReplaceEmptyPlace {
     public String replaceSpace(String s) {
-
+        StringBuilder res = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
+                res.append("%20");
+            } else res.append(c);
+        }
+        return res.toString();
     }
 }
