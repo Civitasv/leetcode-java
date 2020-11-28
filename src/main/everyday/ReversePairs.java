624be058716e6410ef430dd609427bcea39aaa18
husen <2309780282@qq.com>
2020-11-28 21:37:48 +0800

    二叉索引树

---
+++ src/main/everyday/ReversePairs.java
@@ -0,0 +1,45 @@
+package main.everyday;
+
+import main.structure.BIT;
+
+import java.util.*;
+
+/**
+ * Given an array nums, we call (i, j) an important reverse pair if i < j and nums[i] > 2*nums[j].
+ * <p>
+ * You need to return the number of important reverse pairs in the given array.
+ */
+public class ReversePairs {
+    public int reversePairs(int[] nums) {
+        int res = 0, n = nums.length;
+        int[] copy = Arrays.copyOf(nums, n);
+        BIT bit = new BIT(n);
+
+        Arrays.sort(copy);
+
+        for (int ele : nums) {
+            res += bit.query(index(copy, 2L * ele + 1));
+            bit.insert(1, index(copy, ele));
+        }
+
+        return res;
+    }
+
+    private int index(int[] arr, long val) {
+        int l = 0, r = arr.length - 1, m;
+
+        while (l <= r) {
+            m = l + ((r - l) >> 1);
+
+            if (arr[m] >= val) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+
+        return l + 1;
+    }
+}
+
+
