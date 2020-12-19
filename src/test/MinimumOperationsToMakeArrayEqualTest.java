162c01c51b68d73788b045ba5d08b0a82dc79176
Civitasv <2309780282@qq.com>
2020-12-19 21:41:54 +0800

    使数组中所有元素相等的最小操作数

---
+++ src/test/MinimumOperationsToMakeArrayEqualTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import main.MinimumOperationsToMakeArrayEqual;
+import org.junit.jupiter.api.Test;
+
+class MinimumOperationsToMakeArrayEqualTest {
+
+    @Test
+    void test() {
+        MinimumOperationsToMakeArrayEqual equal = new MinimumOperationsToMakeArrayEqual();
+        System.out.println(equal.minOperations(0));
+    }
+}
