8dc00d5e7e4a5390219491428dda5e6a0ee5eb24
Civitasv <2309780282@qq.com>
2020-12-21 10:27:02 +0800

    使用最小花费爬楼梯

--- src/main/everyday/MinCostClimbingStairs.java
+++ src/main/everyday/MinCostClimbingStairs.java
@@ -11,7 +11,7 @@ package main.everyday;
  */
 public class MinCostClimbingStairs {
     public int minCostClimbingStairs(int[] cost) {
-        // dp(i) = min(dp(i-1)+cost(i-1), dp(i-2)+cost(i-2)) i=0
+        // dp(i) = min(dp(i-1)+cost(i-1), dp(i-2)+cost(i-2)) i=0 i=1 dp[i] = 0 dp[n]表示跨上第n个阶梯 楼层顶部就表示第n+1个
         int[] dp = new int[cost.length + 1];
         dp[0] = 0;
         dp[1] = 0;
