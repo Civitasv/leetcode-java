9d31b2d176c3b9b634d127c7929a6c1a09db1366
Civitasv <sen.hu@whu.edu.cn>
2021-02-02 13:36:18 +0800

    替换后的最长重复字符

--- src/main/everyday/LongestRepeatingCharacterReplacement.java
+++ src/main/everyday/LongestRepeatingCharacterReplacement.java
@@ -1,36 +1,25 @@
 package main.everyday;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 替换后的最长重复字符
  */
 public class LongestRepeatingCharacterReplacement {
     public int characterReplacement(String s, int k) {
-        int max = 0;
-        Map<Character, Integer> map = new LinkedHashMap<>();
-        int left = 0, right = 0, N = s.length();
+        int[] alphabets = new int[26];
+        int left = 0, right = 0, N = s.length(), maxN = 0;
         while (right < N) {
-            char c = s.charAt(right);
-            if (map.containsKey(c)) {
-                map.put(c, map.get(c) + 1);
-            } else map.put(c, 1);
+            alphabets[s.charAt(right) - 'A']++;
             // 判断是否符合题意
-            int maxCn = Collections.max(map.values());
-            while (right - left + 1 - maxCn > k) {
+            maxN = Math.max(maxN, alphabets[s.charAt(right) - 'A']);
+            if (right - left + 1 - maxN > k) {
                 // 移动左指针
-                char lc = s.charAt(left);
-                if (map.containsKey(lc)) {
-                    map.put(lc, map.get(lc) - 1);
-                }
+                alphabets[s.charAt(left) - 'A']--;
                 left++;
             }
-            max = Math.max(max, right - left + 1);
             right++;
         }
-        return max;
+        return right - left;
     }
 }
