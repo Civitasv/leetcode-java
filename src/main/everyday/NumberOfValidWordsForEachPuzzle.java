65241f7c6698cb36c2916eb7b16d124e0c800b7c
Civitasv <sen.hu@whu.edu.cn>
2021-02-26 10:47:56 +0800

    猜字谜

--- src/main/everyday/NumberOfValidWordsForEachPuzzle.java
+++ src/main/everyday/NumberOfValidWordsForEachPuzzle.java
@@ -12,31 +12,29 @@ public class NumberOfValidWordsForEachPuzzle {
     public List<Integer> findNumOfValidWords(String[] words, String[] puzzles) {
         Map<Integer, Integer> map = new HashMap<>();
         for (String word : words) {
-            int mask = 0;
+            int bs = 0;
             for (int i = 0; i < word.length(); i++) {
-                mask |= (1 << word.charAt(i) - 'a');
+                bs |= (1 << word.charAt(i) - 'a');
             }
             // 如果mask的1位数超过7，一定无法作为谜底
-            if (Integer.bitCount(mask) <= 7)
-                map.put(mask, map.getOrDefault(mask, 0) + 1);
+            if (Integer.bitCount(bs) <= 7)
+                map.put(bs, map.getOrDefault(bs, 0) + 1);
         }
         List<Integer> result = new ArrayList<>();
         for (String puzzle : puzzles) {
-            int total = 0;
-            // 枚举，第一个字符不变，其它6个字符枚举，共2e6种
-            for (int c = 0; c < (1 << 6); c++) {
-                int mask = 0;
+            int count = 0;
+            // 枚举，第一个字符不变，其它6个字符枚举，共2^6种
+            for (int c = 0; c < 64; c++) {
+                int bs = 0;
                 for (int i = 0; i < 6; ++i) {
                     if ((c & (1 << i)) != 0) {
-                        mask |= (1 << (puzzle.charAt(i + 1) - 'a'));
+                        bs |= (1 << (puzzle.charAt(6 - i) - 'a'));
                     }
                 }
-                mask |= (1 << (puzzle.charAt(0) - 'a'));
-                if (map.containsKey(mask)) {
-                    total += map.get(mask);
-                }
+                bs |= (1 << (puzzle.charAt(0) - 'a'));
+                count += map.getOrDefault(bs, 0);
             }
-            result.add(total);
+            result.add(count);
         }
         return result;
     }
