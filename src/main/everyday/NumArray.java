abebbdcec7ee431634d7f3bad1c77065d7f20a79
Civitasv <sen.hu@whu.edu.cn>
2021-03-01 19:31:30 +0800

    区域和检索 - 数组不可变

--- src/main/everyday/NumArray.java
+++ src/main/everyday/NumArray.java
@@ -4,20 +4,17 @@ package main.everyday;
  * 区域和检索 - 数组不可变
  */
 public class NumArray {
-    private int[] dp;
+    private final int[] dp;
 
     public NumArray(int[] nums) {
         int n = nums.length;
-        if (n == 0) return;
-        dp = new int[n];
-        dp[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            dp[i] = dp[i - 1] + nums[i];
+        dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i - 1] + nums[i - 1];
         }
     }
 
     public int sumRange(int i, int j) {
-        if (i == 0) return dp[j];
-        else return dp[j] - dp[i - 1];
+        return dp[j + 1] - dp[i];
     }
 }
