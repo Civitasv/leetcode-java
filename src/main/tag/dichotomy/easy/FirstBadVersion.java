82578738209b22d330860a3152977e0cc535ad8e
Civitasv <2309780282@qq.com>
2021-07-10 16:57:33 +0800

    第一个错误的版本

--- src/main/tag/dichotomy/easy/FirstBadVersion.java
+++ src/main/tag/dichotomy/easy/FirstBadVersion.java
@@ -2,6 +2,10 @@ package main.tag.dichotomy.easy;
 
 /**
  * 第一个错误的版本
+ *
+ * 思路：
+ *
+ * 1-n中二分搜索，目的是检索第一个错误的版本，即第一个为false的，若非错误版本，则说明错误还在后面，需要向后遍历，
  */
 public class FirstBadVersion {
     public int firstBadVersion(int n) {
