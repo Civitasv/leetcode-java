d9ce9eea37c0137a720cd6697b16949ee2e0161f
Civitasv <2309780282@qq.com>
2021-07-11 11:11:51 +0800

    H 指数

--- src/main/everyday/HIndex.java
+++ src/main/everyday/HIndex.java
@@ -9,6 +9,9 @@ import java.util.Arrays;
  * <p>
  * 思路2：将数组排序，则对于索引i，有n-i个论文引用数目不低于citations[i]，i个论文引用数目不高于citations[i]，
  * 显然若citations[i]>=h，有n-i个论文引用数目不低于h，i个论文引用数目不高于h，满足条件
+ * <p>
+ * 思路3：使用计数器数组counter，counter[i]表示大于等于i的论文数目，那么i从n到0遍历，若有sum篇大于等于i即引用至少i次，
+ * 则有n-sum篇小于等于i即引用不超过n次，显然此时 sum>=i 时，满足题目条件
  */
 public class HIndex {
     public int hIndex(int[] citations) {
@@ -32,6 +35,25 @@ public class HIndex {
             int h = n - i;
             if (citations[i] >= h)
                 return h;
+        }
+        return 0;
+    }
+
+    private int hIndex3(int[] citations) {
+        int n = citations.length;
+        int[] counter = new int[n + 1]; // 计数器，counter[i]表示大于等于i的论文数目
+        int sum = 0;
+        for (int citation : citations) {
+            if (citation >= n) {
+                counter[n]++;
+            } else {
+                counter[citation]++;
+            }
+        }
+        for (int i = n; i >= 0; i--) {
+            sum += counter[i];
+            if (sum >= i)
+                return i;
         }
         return 0;
     }
