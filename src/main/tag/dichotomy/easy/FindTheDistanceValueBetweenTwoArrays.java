f00734c1aaea89b1c83dffcd222482c45d39dcda
Civitasv <2309780282@qq.com>
2021-07-27 21:02:11 +0800

    两个数组间的距离值

--- src/main/tag/dichotomy/easy/FindTheDistanceValueBetweenTwoArrays.java
+++ src/main/tag/dichotomy/easy/FindTheDistanceValueBetweenTwoArrays.java
@@ -4,6 +4,11 @@ import java.util.Arrays;
 
 /**
  * 两个数组间的距离值
+ * <p>
+ * 思路：对于数组 arr1 中的每个元素，只需要找到数组 arr2 中刚好大于和刚好小于的数字，然后与其进行比较即可
+ * <p>
+ * 因此，使用二分查找，可以获取第一个大于该元素的索引，若存在则索引小于 arr2.length，否则 arr2 中的所有数字均小于
+ * 该元素，此时仅存在比其小的数字，比较一次即可
  */
 public class FindTheDistanceValueBetweenTwoArrays {
     public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
@@ -12,9 +17,9 @@ public class FindTheDistanceValueBetweenTwoArrays {
         for (int num : arr1) {
             int index = binarySearch(arr2, num);
             boolean ok = true;
-            if (index < arr2.length)
+            if (index < arr2.length) // 大于它的
                 ok &= (Math.abs(arr2[index] - num) > d);
-            if (index - 1 >= 0 && index - 1 < arr2.length)
+            if (index - 1 >= 0 && index - 1 < arr2.length) // 小于它的
                 ok &= (Math.abs(arr2[index - 1] - num) > d);
             count += ok ? 1 : 0;
         }
